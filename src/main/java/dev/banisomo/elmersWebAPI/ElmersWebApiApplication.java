7081b8b1ddd9c4e1c25102f92820d29fb4503017
Bani Somo <banisomo2003@gmail.com>
2023-11-13 08:32:24 -0500

    Initialize project

---
+++ src/main/java/dev/banisomo/elmersWebAPI/ElmersWebApiApplication.java
@@ -0,0 +1,13 @@
+package dev.banisomo.elmersWebAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElmersWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElmersWebApiApplication.class, args);
+	}
+
+}
